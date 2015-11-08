cf7da55dd923371e1f4dda5bcb706c877d7ef7c9
steveatwork <steve@twinprime.com>
2015-11-08 13:57:39 -0800

    formattable query, smarter content grouping

--- loader.py
+++ loader.py
@@ -4,11 +4,7 @@ from collections import defaultdict
 from pprint import pprint
 from sqlalchemy import create_engine
 
-import queries
-
-SIGNIFICANT_RECORD_COUNT = 500
-SIGNIFICANT_PERCENTILE_ERROR = .15
-
+import utils
 
 class RedShiftLoader():
 
@@ -16,29 +12,45 @@ class RedShiftLoader():
         self.cursor = None
         self.engine_string = "postgresql+psycopg2://%s:%s@%s:%d/%s" % (
             user, password, endpoint, port, dbname)
-        self.SIGNIFICANT_RECORD_COUNT = 500
+        self.SIGNIFICANT_RECORD_COUNT = 50
         self.SIGNIFICANT_PERCENTILE_ERROR = .15
 
     def Connect(self):
         redshift = create_engine(self.engine_string)
         self.cursor = redshift.connect()
 
-    def Query(self, query):
+    def Query(self, details):
         if not self.cursor:
             self.Connect()
-
-        """hard coded for now"""
-        # handle query somehow? 
-        rs_query = queries.redshift_query.format(start_date='2015-11-02', end_date='2015-11-06', cid=3521, limit=50000)
+        
+        formatter = self.queryScrub(details) 
+        rs_query = utils.redshift_query.format(**formatter)
         self.rows = self.cursor.execute(rs_query).fetchall()
         print 'query complete', "%i rows returned" % len(self.rows)
 
+    def queryScrub(self, details):
+        if 'cid' not in details:
+            exit('specify cid')
+
+        scrubbed = {
+            'cid': details['cid'],
+            'start_date': details.get('start_date', '2015-11-07'),
+            'end_date': details.get('end_date', '2015-11-09'),
+            'limit': details.get('limit', 500)
+            }
+        return scrubbed
+
     def ReduceResults(self):
         hash_grouped = {}
-        id_keys = ['network', 'geo', 'url_domain', 'content_type', 'size']
+        id_keys = ['network', 'geo', 'url_domain', 'size'] #treat content_type separately
+        self.to_add = set()
 
         for row in self.rows:
             hash_string = "&".join(["=".join([key, row[key]]) for key in id_keys])
+            reduced_content = utils.reducedContentType(row['content_type']) 
+            if reduced_content == "oops":
+                self.to_add.add(row['content_type'])
+            hash_string += '&content_type={}'.format(reduced_content)
             tpclass = row['class']
             # hash_info = hash_grouped.get(hash_string, {})
             exists = hash_grouped.get(hash_string, False)
@@ -46,20 +58,19 @@ class RedShiftLoader():
                 hash_info = {'id':{}, 'metrics':{}, 'count': 0}
                 for key in id_keys:
                     hash_info['id'].update({key: row[key]})
+                hash_info['id'].update({'content_type': reduced_content})
                 hash_grouped[hash_string] = hash_info
 
             hash_grouped[hash_string]['count'] += row['bin_count']
             class_exists = hash_grouped[hash_string]['metrics'].get(tpclass, False)
-            if class_exists:
-                #merge bins into existing descriptor
-                hash_grouped[hash_string]['metrics'][tpclass]['bins'].update({row['bin']: row['bin_count']})
+            if class_exists: 
                 hash_grouped[hash_string]['metrics'][tpclass]['count'] += row['bin_count']
+                if tpclass in ['acc', 'byp']:
+                    #merge bins into existing descriptor
+                    hash_grouped[hash_string]['metrics'][tpclass]['bins'].update({row['bin']: row['bin_count']})
             else:
                 #create new class entries
                 class_info = {
-                                 'bins': {
-                                     row['bin'] : row['bin_count']
-                                 },
                                  'medians': {
                                      'fbu': row['fbu'],
                                      'dcu': row['dcu']
@@ -71,6 +82,8 @@ class RedShiftLoader():
                                  },
                                  'count': row['bin_count']
                              }
+                if tpclass in ['acc', 'byp']:
+                    class_info.update({'bins': { row['bin']: row['bin_count'] }})
 
                 hash_grouped[hash_string]['metrics'][row['class']] = class_info
 
@@ -80,12 +93,13 @@ class RedShiftLoader():
         return comparable['num_comparable_records'] >= self.SIGNIFICANT_RECORD_COUNT
 
     def CheckSizePrecision(self, comparable):
-        byp = comparable.get('byp', {})
-        acc = comparable.get('acc', {})
-
-        checks = [((byp.get(perc, None) - acc.get(perc, None)) / byp[perc]) <
-                  SIGNIFICANT_PERCENTILE_ERROR for perc in ['size_25', 'size_50', 'size_75']]
-        # print 'checks', checks
+        byp_sizes = comparable['metrics'].get('byp', {}).get('sizes', {})
+        acc_sizes = comparable['metrics'].get('acc', {}).get('sizes', {})
+        if not acc_sizes and byp_sizes:
+            checks=[False]
+        else:
+            checks = [((byp_sizes[perc] - acc_sizes[perc]) / byp_sizes[perc]) <
+                  self.SIGNIFICANT_PERCENTILE_ERROR for perc in ['size_25', 'size_50', 'size_75']]
         return all(checks)
 
     def SignificantChecks(self, hash_grouped):
@@ -99,19 +113,29 @@ class RedShiftLoader():
             comparable['num_exception_records'] = num_except
             comparable['num_total_records'] = sum([num_comp, num_except])
 
-            if 'acc' in comparable and 'byp' in comparable:
-                comparable['comparability'] = all(
-                    [self.CheckSampleSize(comparable), self.CheckSizePrecision(comparable)])
+            if 'acc' in comparable['metrics'] and 'byp' in comparable['metrics']:
+                if self.CheckSampleSize(comparable):
+                    if self.CheckSizePrecision(comparable):
+                        comparable['comparability'] = True
+                    else:
+                        comparable['comparability'] = False
+                        comparable['fail_reason'] = 'size comparison'
+                else:
+                    comparable['comparability'] = False
+                    comparable['fail_reason'] = 'sample size'
             else:
                 comparable['comparability'] = False
+                comparable['fail_reason'] = 'acc/byp samples'
 
+            if comparable['comparability']:
+                print comparable
             comparables.append(comparable)
-        pprint(comparables[-50:])
-        exit()
         return comparables
 
     def FinalForm(self, comparable_list):
         reduced_comparables = []
+
+        goods, bads = [], []
         for comparable in comparable_list:
             reduced = {
                 'network': comparable['id']['network'],
@@ -123,22 +147,62 @@ class RedShiftLoader():
                 'num_comparable_records': comparable['num_comparable_records'],
                 'num_exception_records': comparable['num_exception_records'],
                 'comparability': comparable['comparability'],
+                'fail_reason': comparable.get('fail_reason', None)
+                }
+            
+            byp_metrics, acc_metrics = comparable['metrics'].get('byp', {}), comparable['metrics'].get('acc', {})
+            metrics = {
+                'bins': {
+                    'acc': acc_metrics.get('bins', {}),
+                    'byp': byp_metrics.get('bins', {})
+                    },
+                'percentiles': {
+                    'acc': {
+                        'dcu_median': acc_metrics.get('medians', {}).get('dcu', 0)
+                        },
+                    'byp': {
+                        'dcu_median': byp_metrics.get('medians', {}).get('dcu', 0)
+                        }
+                    }
             }
+            reduced.update({'metrics': metrics})
+
             if comparable['comparability']:
-                gain = comparable['byp']['dcu'] / comparable['acc']['dcu'] - 1
+                gain = comparable['metrics']['byp']['medians']['dcu'] / comparable['metrics']['acc']['medians']['dcu'] - 1
+                goods.append(comparable)
             else:
-                gain = 0
+                gain = None
+                bads.append(comparable)
             reduced.update({'gain': gain})
             reduced_comparables.append(reduced)
-
+        out = "{}: {}"
+        print out.format('goods', len(goods)), out.format('bads', len(bads))
         return reduced_comparables
 
     def LoadToProduction(self):
-        for entry in self.comparables[:10]:
-            print entry
+        print 'this many', len(self.comparables)
+        urls = set()
+
+        num_good, num_bad = 0, 0
+        reasons = defaultdict(lambda : defaultdict(int))
+        for entry in self.comparables: 
+            urls.add(entry['url_domain'])
+            #print entry
             #rr = models.ReducedRow(**entry)
-            #db.session.add(rr)
+            if entry['fail_reason']:
+                reasons[entry['fail_reason']]['count'] += 1
+                reasons[entry['fail_reason']]['sample'] += entry['num_total_records']
+                num_bad += entry['num_total_records']
+            else:
+                num_good += entry['num_total_records']
+   #db.session.add(rr)
         #db.session.commit()
+        print reasons
+        print 'good total', num_good, 'bad total', num_bad
+        if self.to_add:
+            print 'to_add', self.to_add
+                
+        print 'urls', urls
 
     def Test(self):
         if not self.cursor:
@@ -147,22 +211,15 @@ class RedShiftLoader():
 
 if __name__ == "__main__":
     import credentials
+    import sys
+    
+    cid = sys.argv[1]
+
     tplog = RedShiftLoader(credentials.redshift_endpoint, credentials.redshift_dbname,
                            credentials.redshift_port, credentials.redshift_user, credentials.redshift_pass)
-    tplog.Query('blah')
+    tplog.Query({'cid': cid, 'start_date': '2015-11-06', 'end_date': '2015-11-09', 'limit': 50000})
     tplog.ReduceResults()
     tplog.LoadToProduction()
     #tplog.Test()
     #for row in tplog.rows[:20]:
     #    print row['bin']
-'''
-to_print = []
-for key, value in comps.iteritems():
-        if not value.get('num_records', False):
-                print 'oops?', value
-        else:
-                to_print.append({key: value})
-print 'to_print', to_print
-pprint(sorted(to_print, key=lambda x: x['num_records'], reverse=True)) 
-#for row in prod_results:
-#    print row'''
---
+++ utils.py
@@ -0,0 +1,216 @@
+
+redshift_query = \
+"""select   partition1 as network, 
+         partition2 as geo, 
+         partition3 as url_domain, 
+         partition4 as content_type, 
+         partition5 as size, 
+         partition6 as bin, 
+         count(*) AS bin_count, 
+         class, 
+         max(median_fbu)   AS fbu, 
+         max(median_dcu)   AS dcu, 
+         max(extra0)       AS size_25, 
+         max(extra1)       AS size_50, 
+         max(extra2)       AS size_75 
+FROM     ( 
+                  SELECT   network      AS partition1, 
+                           geo          AS partition2, 
+                           url_domain   AS partition3, 
+                           content_type AS partition4, 
+                           CASE 
+                                    WHEN size < 10000 THEN 'Small' 
+                                    WHEN size < 30000 THEN 'Medium' 
+                                    WHEN size < 50000 THEN 'Medium+' 
+                                    WHEN size < 200000 THEN 'Large' 
+                                    ELSE 'X-Large' 
+                           END AS partition5, 
+                           50*(1+floor(dcu/50)) as bin,
+                           CASE                                     
+                                    WHEN dcu < 50 THEN '50'
+                                    WHEN dcu < 100 THEN '100'
+                                    WHEN dcu < 150 THEN '150'
+                                    WHEN dcu < 200 THEN '200'
+                                    WHEN dcu < 250 THEN '250'
+                                    WHEN dcu < 300 THEN '300'
+                                    WHEN dcu < 350 THEN '350'
+                                    WHEN dcu < 400 THEN '400'
+                                    WHEN dcu < 450 THEN '450'
+                                    WHEN dcu < 500 THEN '500'
+                                    WHEN dcu < 550 THEN '550'
+                                    WHEN dcu < 600 THEN '600'
+                                    WHEN dcu < 650 THEN '650'
+                                    WHEN dcu < 700 THEN '700'
+                                    WHEN dcu < 750 THEN '750'
+                                    WHEN dcu < 800 THEN '800'
+                                    WHEN dcu < 850 THEN '850'
+                                    WHEN dcu < 900 THEN '900'
+                                    WHEN dcu < 950 THEN '950'
+                                    WHEN dcu < 1000 THEN '1000'
+                                    WHEN dcu < 1050 THEN '1050'
+                                    WHEN dcu < 1100 THEN '1100'
+                                    WHEN dcu < 1150 THEN '1150'
+                                    WHEN dcu < 1200 THEN '1200'
+                                    WHEN dcu < 1250 THEN '1250'
+                                    WHEN dcu < 1300 THEN '1300'
+                                    WHEN dcu < 1350 THEN '1350'
+                                    WHEN dcu < 1400 THEN '1400'
+                                    WHEN dcu < 1450 THEN '1450'
+                                    WHEN dcu < 1500 THEN '1500'
+                                    WHEN dcu < 1550 THEN '1550'
+                                    WHEN dcu < 1600 THEN '1600'
+                                    WHEN dcu < 1650 THEN '1650'
+                                    WHEN dcu < 1700 THEN '1700'
+                                    WHEN dcu < 1750 THEN '1750'
+                                    WHEN dcu < 1800 THEN '1800'
+                                    WHEN dcu < 1850 THEN '1850'
+                                    WHEN dcu < 1900 THEN '1900'
+                                    WHEN dcu < 1950 THEN '1950'
+                                    WHEN dcu < 2000 THEN '2000'
+                                    WHEN dcu < 2050 THEN '2050'
+                                    WHEN dcu < 2100 THEN '2100'
+                                    WHEN dcu < 2150 THEN '2150'
+                                    WHEN dcu < 2200 THEN '2200'
+                                    WHEN dcu < 2250 THEN '2250'
+                                    WHEN dcu < 2300 THEN '2300'
+                                    WHEN dcu < 2350 THEN '2350'
+                                    WHEN dcu < 2400 THEN '2400'
+                                    WHEN dcu < 2450 THEN '2450'
+                                    WHEN dcu < 2500 THEN '2500'
+                                    WHEN dcu < 2550 THEN '2550'
+                                    WHEN dcu < 2600 THEN '2600'
+                                    WHEN dcu < 2650 THEN '2650'
+                                    WHEN dcu < 2700 THEN '2700'
+                                    WHEN dcu < 2750 THEN '2750'
+                                    WHEN dcu < 2800 THEN '2800'
+                                    WHEN dcu < 2850 THEN '2850'
+                                    WHEN dcu < 2900 THEN '2900'
+                                    WHEN dcu < 2950 THEN '2950'
+                                    WHEN dcu < 3000 THEN '3000'
+                                    WHEN dcu < 3050 THEN '3050'
+                                    WHEN dcu < 3100 THEN '3100'
+                                    WHEN dcu < 3150 THEN '3150'
+                                    WHEN dcu < 3200 THEN '3200'
+                                    WHEN dcu < 3250 THEN '3250'
+                                    WHEN dcu < 3300 THEN '3300'
+                                    WHEN dcu < 3350 THEN '3350'
+                                    WHEN dcu < 3400 THEN '3400'
+                                    WHEN dcu < 3450 THEN '3450'
+                                    WHEN dcu < 3500 THEN '3500'
+                                    WHEN dcu < 3550 THEN '3550'
+                                    WHEN dcu < 3600 THEN '3600'
+                                    WHEN dcu < 3650 THEN '3650'
+                                    WHEN dcu < 3700 THEN '3700'
+                                    WHEN dcu < 3750 THEN '3750'
+                                    WHEN dcu < 3800 THEN '3800'
+                                    WHEN dcu < 3850 THEN '3850'
+                                    WHEN dcu < 3900 THEN '3900'
+                                    WHEN dcu < 3950 THEN '3950'
+                                    WHEN dcu < 4000 THEN '4000'
+                                    WHEN dcu >= 4000 THEN '>4000' 
+                           END AS partition6,
+                           class, 
+                           median(fbu) OVER (partition BY network, geo, url_domain, content_type,
+                           CASE 
+                                    WHEN size < 10000 THEN 'Small' 
+                                    WHEN size < 30000 THEN 'Medium' 
+                                    WHEN size < 50000 THEN 'Medium+' 
+                                    WHEN size < 200000 THEN 'Large' 
+                                    ELSE 'X-Large' 
+                           END, 
+                           class) AS median_fbu, 
+                           median(dcu) OVER (partition BY network, geo, url_domain, content_type,
+                           CASE 
+                                    WHEN size < 10000 THEN 'Small' 
+                                    WHEN size < 30000 THEN 'Medium' 
+                                    WHEN size < 50000 THEN 'Medium+' 
+                                    WHEN size < 200000 THEN 'Large' 
+                                    ELSE 'X-Large' 
+                           END, 
+                           class) AS median_dcu, 
+                           percentile_cont(0.25) within GROUP (ORDER BY size) OVER (partition BY network, geo, url_domain, content_type,
+                           CASE 
+                                    WHEN size < 10000 THEN 'Small' 
+                                    WHEN size < 30000 THEN 'Medium' 
+                                    WHEN size < 50000 THEN 'Medium+' 
+                                    WHEN size < 200000 THEN 'Large' 
+                                    ELSE 'X-Large' 
+                           END, 
+                           class) AS extra0, 
+                           median(size) OVER (partition BY network, geo, url_domain, content_type,
+                           CASE 
+                                    WHEN size < 10000 THEN 'Small' 
+                                    WHEN size < 30000 THEN 'Medium' 
+                                    WHEN size < 50000 THEN 'Medium+' 
+                                    WHEN size < 200000 THEN 'Large' 
+                                    ELSE 'X-Large' 
+                           END, 
+                           class) AS extra1, 
+                           percentile_cont(0.75) within GROUP (ORDER BY size) OVER (partition BY network, geo, url_domain, content_type,
+                           CASE 
+                                    WHEN size < 10000 THEN 'Small' 
+                                    WHEN size < 30000 THEN 'Medium' 
+                                    WHEN size < 50000 THEN 'Medium+' 
+                                    WHEN size < 200000 THEN 'Large' 
+                                    ELSE 'X-Large' 
+                           END, 
+                           class) AS extra2 
+                  FROM     tplog 
+                  WHERE    datetime > '{start_date}'
+                  AND      datetime < '{end_date}'
+                  AND      cid = {cid} 
+                  AND      network IS NOT NULL 
+                  AND      geo IS NOT NULL 
+                  AND      url_domain IS NOT NULL 
+                  AND      content_type IS NOT NULL 
+                  AND      size IS NOT NULL 
+                  AND      dcu IS NOT NULL ) AS nested 
+GROUP BY partition1, 
+         partition2, 
+         partition3, 
+         partition4, 
+         partition5,
+         partition6,  
+         class 
+ORDER BY partition1, 
+         partition2, 
+         partition3, 
+         partition4, 
+         partition5,
+         partition6
+LIMIT {limit};"""
+
+content_types = {
+    '': 'none',
+    'application/font-woff': 'font',
+    'application/javascript': 'json',
+    'application/json': 'json',
+    'application/octet-stream': 'binary',
+    'application/x-font-ttf': 'font',
+    'application/x-font-woff': 'font',
+    'application/x-javascript': 'application',
+    'application/xml': 'application',
+    'font/opentype': 'font',
+    'font/ttf': 'font',
+    'image': 'image',
+    'image/gif': 'image',
+    'image/jpeg': 'image',
+    'image/jpg': 'image',
+    'image/png': 'image',
+    'image/webp': 'image',
+    'image/svg+xml': 'image',
+    'multipart': 'multipart',
+    'text': 'text',
+    'text/css': 'text', 
+    'text/html': 'text',
+    'text/javascript': 'text',
+    'text/plain': 'text'
+    }
+
+def reducedContentType(content_type):
+    content_type = content_type.split(';')[0]
+    if content_type in content_types:
+        ctype = content_types[content_type]
+    else:
+        ctype = content_types.get(content_type.split('/')[0], "oops") #try this for a while and see what happens
+    return ctype
