0f722b5caa4d641b8d9cb9dd9dbe2c1066417e74
steveatwork <steve@twinprime.com>
2015-12-16 12:08:05 -0800

    changed the location of config to help with db_create

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import configs
+import os
 
 app = Flask(__name__)
+
+tp_env = os.environ.get('TP_ENV_TYPE', 'dev')
+if tp_env == 'prod':
+    app.config.from_object(configs.Production)
+else:
+    app.config.from_object(configs.Config)
+
 db = SQLAlchemy(app)
 
 from app import models, views
--- run.py
+++ run.py
@@ -1,14 +1,3 @@
 #!flask-venv/bin/python
 from app import app
-
-import configs
-import os
-
-tp_env = os.environ.get('TP_ENV_TYPE', 'dev')
-
-if tp_env == 'prod':
-    app.config.from_object(configs.Production)
-    app.run(host="0.0.0.0")
-else:
-    app.config.from_object(configs.Config)
-    app.run()
+app.run(host=app.config.get('HOST', 'localhost'))
