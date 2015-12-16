53e128a81b3332a63503ec0e293465408f0d38da
steveatwork <steve@twinprime.com>
2015-12-16 10:56:13 -0800

    make host public on prod

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ tp_env = os.environ.get('TP_ENV_TYPE', 'dev')
 
 if tp_env == 'prod':
     app.config.from_object(configs.Production)
+    app.run(host="0.0.0.0")
 else:
     app.config.from_object(configs.Config)
-
-app.run()
+    app.run()
