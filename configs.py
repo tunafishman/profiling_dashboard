6dca28bb77f5eeb9373771009d3a63dbc6d14967
steveatwork <steve@twinprime.com>
2015-12-16 10:25:43 -0800

    db location fixes

--- configs.py
+++ configs.py
@@ -3,9 +3,11 @@ class Config(object):
     SERVER_NAME = "localhost:5001"
     API_URL = "localhost"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://localhost/comparables"
 
 class Production(object):
     DEBUG = False
     SERVER_NAME = "0.0.0.0:5001"
     API_URL = "web-01"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://comparables"
